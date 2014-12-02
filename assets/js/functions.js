34f091b30ae793ae9b539fc88019c1998cbc7e51
Caleb Sylvest <caleb.sylvest@gmail.com>
2014-12-01 22:49:58 -0600

    update everything.

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -4,11 +4,31 @@
 	/* trigger when page is ready */
 	$(document).ready(function (){
 	
-		// your functions go here
+		// listen for click of email signup button
 		$('#email-button').click(function() {
 			$('#email-button').addClass("active");
 			$('#mc-embedded-subscribe').addClass("show");
 			$(this).attr('placeholder','Email Address...');
+		});
+
+		/*$('.bold-button').click(function() {
+			$(this).toggleClass("active");
+			$('.example-highlight').addClass("bold");			
+		});*/
+
+		$('.bold-button').click(function() {
+			$('.bold-button').toggleClass("active");
+			$('.example-highlight').toggleClass("bold");			
+		});
+
+		$('.italic-button').click(function() {
+			$('.italic-button').toggleClass("active");
+			$('.example-highlight').toggleClass("italic");
+		});
+
+		$('.underline-button').click(function() {
+			$('.underline-button').toggleClass("active");
+			$('.example-highlight').toggleClass("underline");
 		});
 	
 	});
