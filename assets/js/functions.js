1e2220955a6d1fad252dc9ce51b594fdce4e6d30
Caleb Sylvest <caleb.sylvest@gmail.com>
2014-12-06 13:06:41 -0600

    fix specificity issue with email buttons.

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -4,24 +4,26 @@
 	/* trigger when page is ready */
 	$(document).ready(function (){
 	
-		// listen for click of email signup button
+		// listen for click of email signup button. two buttons on page.
 		$('.email-signup-button').click(function() {
-			$('.email-signup-button').addClass("active");
-			$('.email-signup-submit').addClass("show");
-			$(this).attr('placeholder','Email Address...');
+			$(this).addClass("active"); // add class to button when clicked
+			$(this).siblings('.email-signup-submit').addClass("show"); // add class to submit button
+			$(this).attr('placeholder','Email Address...'); // change text in input field
 		});
 
-		// faux tooltip menu
+		// faux tooltip menu bold button
 		$('.bold-button').click(function() {
 			$('.bold-button').toggleClass("active");
 			$('.example-highlight').toggleClass("bold");			
 		});
 
+		// faux tooltip menu italic button
 		$('.italic-button').click(function() {
 			$('.italic-button').toggleClass("active");
 			$('.example-highlight').toggleClass("italic");
 		});
 
+		// faux tooltip menu underline button
 		$('.underline-button').click(function() {
 			$('.underline-button').toggleClass("active");
 			$('.example-highlight').toggleClass("underline");
